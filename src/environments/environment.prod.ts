b5c6b70a017dae80ae3840a1fbe931fdf15a3d67
JulienNedellec <nedellec.julien.78@gmail.com>
2024-09-25 15:39:35 +0200

    Améliorer le chemin de déploiement Docker pour le site Arcadia Zoo en utilisant un chemin de clé personnalisé

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://arcadia.nedellec-julien.fr/api', // L'URL de l'API pour l'environnement de production
+  frontUrl: 'https://arcadia.nedellec-julien.fr',
+};
