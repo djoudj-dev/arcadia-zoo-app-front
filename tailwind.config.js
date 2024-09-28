84f663dd01393b52847c64ced86dfcd2841912b9
JulienNedellec <nedellec.julien.78@gmail.com>
2024-09-28 16:30:19 +0200

    update: mise a jour du fichier tailwind.config.js avec font Family et ajout de colors dans extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: { },
-    colors: {
-      primary: '#FFFFFF',
-      secondary: '#A3B583',
-      tertiary: '#557A46',
-      quaternary: '#7A9F61',
-      quinary: '#0E1805',
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+      colors: {
+        primary: '#FFFFFF',
+        secondary: '#A3B583',
+        tertiary: '#557A46',
+        quaternary: '#7A9F61',
+        quinary: '#0E1805',
+      },
     },
   },
   plugins: [],
