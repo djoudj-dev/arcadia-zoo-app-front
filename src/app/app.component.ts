118aaa6ec5dcd5a6d8dec2ce7aa85f6dd16bbc50
JulienNedellec <nedellec.julien.78@gmail.com>
2024-09-29 16:03:00 +0200

    feature: création zoo-intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/components/header/navbar/nav.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './features/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, BrowserModule, BrowserAnimationsModule],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
