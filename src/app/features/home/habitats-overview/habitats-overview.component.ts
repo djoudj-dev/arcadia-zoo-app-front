00bf77ebe2c88ffb23e426e0f8c2205e0b6c2c43
JulienNedellec <nedellec.julien.78@gmail.com>
2024-09-29 17:30:19 +0200

    update: ajustement de la section habitats

--- src/app/features/home/habitats-overview/habitats-overview.component.ts
+++ src/app/features/home/habitats-overview/habitats-overview.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-habitats-overview',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './habitats-overview.component.html',
-  styleUrl: './habitats-overview.component.css'
+  styleUrl: './habitats-overview.component.css',
 })
-export class HabitatsOverviewComponent {
-
-}
+export class HabitatsOverviewComponent {}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ZooIntroComponent } from './zoo-intro/zoo-intro.component';
+import { HabitatsOverviewComponent } from './habitats-overview/habitats-overview.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ZooIntroComponent],
+  imports: [ZooIntroComponent, HabitatsOverviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
