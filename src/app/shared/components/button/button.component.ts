782bf8330b4b0a0c02e0543fa644332fd14848c8
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-01 17:46:31 +0200

    features: Component services

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -9,9 +9,10 @@ import { CommonModule } from '@angular/common';
     [ngClass]="{
       'bg-tertiary hover:bg-inherit': variant === 'tertiary',
       'bg-primary hover:bg-primary-dark': variant === 'primary',
-      'bg-quaternary hover:bg-quaternary-dark': variant === 'quaternary'
+      'bg-quaternary hover:bg-quaternary-dark': variant === 'quaternary',
+      'rounded-t-lg': !noRounded
     }"
-    class="focus:outline-none text-white text-base font-sans w-full py-2 text-center rounded-t-lg"
+    class="focus:outline-none text-white text-base font-sans w-full py-2 text-center"
   >
     {{ text }}
   </button> `,
@@ -20,4 +21,5 @@ export class ButtonComponent {
   @Input() text: string = 'Button';
   @Input() type: string = 'button';
   @Input() variant: 'primary' | 'quaternary' | 'tertiary' = 'primary';
+  @Input() noRounded: boolean = false;
 }
