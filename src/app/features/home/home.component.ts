294f031d6f23fcee333ff1601d72b21eb4bc0d29
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-01 16:03:08 +0200

    update: refactor images

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { ZooIntroComponent } from './zoo-intro/zoo-intro.component';
 import { HabitatsOverviewComponent } from './habitats-overview/habitats-overview.component';
+import { ServicesOverviewComponent } from './services-overview/services-overview.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ZooIntroComponent, HabitatsOverviewComponent],
+  imports: [
+    ZooIntroComponent,
+    HabitatsOverviewComponent,
+    ServicesOverviewComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
