1ffc0d6d533ebd8d57b14316056ca3a59e4c16ff
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-03 21:35:17 +0200

    features: Formulaire d'avis ajouter

--- src/app/features/reviews/form-review/form-review.component.ts
+++ src/app/features/reviews/form-review/form-review.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-form-review',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ButtonComponent],
   templateUrl: './form-review.component.html',
-  styleUrl: './form-review.component.css'
+  styleUrl: './form-review.component.css',
 })
-export class FormReviewComponent {
-
-}
+export class FormReviewComponent {}
