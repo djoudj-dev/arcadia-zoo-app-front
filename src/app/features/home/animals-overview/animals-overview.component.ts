84c4e7e68f14f35da875d7069cf10c44d46b8bd4
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-07 17:28:19 +0200

    feature: vréation component

---
+++ src/app/features/home/animals-overview/animals-overview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animals-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './animals-overview.component.html',
+})
+export class AnimalsOverviewComponent {}
