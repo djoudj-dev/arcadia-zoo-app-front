0c13a4be8f2cb17d7f2584aaf72f8806f444b795
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-07 17:09:07 +0200

    update: refactor de l'affichage des avis

--- src/app/features/reviews/list-review/list-review.component.ts
+++ src/app/features/reviews/list-review/list-review.component.ts
@@ -1,36 +1,59 @@
 import { DatePipe } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Review } from './review.model';
 import { REVIEW } from './list-mock.component';
 import { RateComponent } from '../../../shared/components/rate/rate.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-list-review',
   templateUrl: './list-review.component.html',
   styleUrl: './list-review.component.css',
   standalone: true,
-  imports: [DatePipe, RateComponent],
+  imports: [DatePipe, RateComponent, ButtonComponent],
 })
 export class ListReviewComponent {
   @Input() stars: number[] = [1, 2, 3, 4, 5];
-
-  @ViewChild('scrollContainer', { read: ElementRef })
-  scrollContainer!: ElementRef;
+  @Input() rating: number = 0;
+  @Output() ratingChange: EventEmitter<number> = new EventEmitter<number>();
   reviews: Review[] = [];
+  showModal: boolean = false;
+  currentReviewIndex = 0;
+  isModalOpen: boolean = false;
+  productRating: number = 3;
 
   ngOnInit() {
     this.reviews = REVIEW;
   }
 
-  // Fonction pour faire défiler vers la gauche
-  scrollLeft(): void {
-    const container = this.scrollContainer.nativeElement;
-    container.scrollBy({ left: -600, behavior: 'smooth' });
+  rate(star: number) {
+    this.rating = star;
+    this.ratingChange.emit(this.rating);
   }
 
-  // Fonction pour faire défiler vers la droite
-  scrollRight(): void {
-    const container = this.scrollContainer.nativeElement;
-    container.scrollBy({ left: 600, behavior: 'smooth' });
+  trackByFn(index: number) {
+    return index;
+  }
+
+  // Fonction pour afficher l'avis précédent
+  previousReview(): void {
+    if (this.currentReviewIndex > 0) {
+      this.currentReviewIndex--;
+    }
+  }
+
+  // Fonction pour afficher l'avis suivant
+  nextReview(): void {
+    if (this.currentReviewIndex < this.reviews.length - 1) {
+      this.currentReviewIndex++;
+    }
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
   }
 }
