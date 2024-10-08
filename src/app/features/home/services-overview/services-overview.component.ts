0d46ae4a99350ded94d92d9f163011a2569c7881
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-08 20:41:23 +0200

    update: mise a jour du hover pour la taille mobile

--- src/app/features/home/services-overview/services-overview.component.ts
+++ src/app/features/home/services-overview/services-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
@@ -7,4 +7,24 @@ import { ButtonComponent } from '../../../shared/components/button/button.compon
   imports: [ButtonComponent],
   templateUrl: './services-overview.component.html',
 })
-export class ServicesOverviewComponent {}
+export class ServicesOverviewComponent {
+  @ViewChild('service1', { static: true }) service1!: ElementRef;
+  @ViewChild('service2', { static: true }) service2!: ElementRef;
+  @ViewChild('service3', { static: true }) service3!: ElementRef;
+
+  ngAfterViewInit() {
+    this.addTouchListeners(this.service1);
+    this.addTouchListeners(this.service2);
+    this.addTouchListeners(this.service3);
+  }
+
+  addTouchListeners(element: ElementRef) {
+    const el = element.nativeElement;
+    el.addEventListener('touchstart', () => {
+      el.querySelector('.overlay').classList.add('opacity-100');
+    });
+    el.addEventListener('touchend', () => {
+      el.querySelector('.overlay').classList.remove('opacity-100');
+    });
+  }
+}
