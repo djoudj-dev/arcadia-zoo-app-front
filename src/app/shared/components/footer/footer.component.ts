b87e6bb637a97c98d6e84c6dfda0b8bbba44422c
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-09 08:51:13 +0200

    Update modal display in footer component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -11,10 +11,12 @@ export class FooterComponent {
   openModal() {
     const modal = document.getElementById('modal');
     modal?.classList.remove('hidden');
+    modal?.classList.add('flex');
   }
 
   closeModal() {
     const modal = document.getElementById('modal');
     modal?.classList.add('hidden');
+    modal?.classList.remove('flex');
   }
 }
