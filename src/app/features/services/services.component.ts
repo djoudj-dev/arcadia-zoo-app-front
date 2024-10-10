255297a76926986aca90d1cc07bb7d8318b585b9
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-10 15:32:00 +0200

    Refactorisation: Mise à jour des routes pour la page ServicesOverview et Services

--- src/app/features/services/services.component.ts
+++ src/app/features/services/services.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { Service } from '../../core/models/service.model';
 import { SERVICES } from '../../reviews/mocks/services-mock.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './services.component.html',
 })
 export class ServicesComponent {
   services: Service[] = SERVICES;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
