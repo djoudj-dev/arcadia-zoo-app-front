c0e08f6baab0356b01fc11551e8a057f940fb6fc
JulienNedellec <nedellec.julien.78@gmail.com>
2024-10-10 15:08:57 +0200

    Ajout de la page Services et mise à jour des routes

---
+++ src/app/reviews/mocks/services-mock.component.ts
@@ -0,0 +1,20 @@
+export const SERVICES = [
+  {
+    id: 1,
+    name: 'Restaurant',
+    description: 'Restaurant du zoo',
+    image: 'images/img_services/restaurant.webp',
+  },
+  {
+    id: 2,
+    name: 'Visite guidée',
+    description: 'Visite guidée du zoo',
+    image: 'images/img_services/visite.webp',
+  },
+  {
+    id: 3,
+    name: 'Visite en petit train',
+    description: 'Visite en petit train du zoo',
+    image: 'images/img_services/train.webp',
+  },
+];
