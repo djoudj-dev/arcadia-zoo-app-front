cff89403c37515808c3692f9bbf118b2591cb27b
djoudj-dev <nedellec.julien.78@gmail.com>
2024-10-17 10:40:39 +0200

    Refactor dashbord: gestion de compte + button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,19 +7,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-button',
   template: `<button
     [type]="type"
-    [ngClass]="{
-      'bg-primary hover:bg-primary/80 focus:bg-primary/70': color === 'primary',
-      'bg-secondary hover:bg-secondary/80 focus:bg-secondary/70':
-        color === 'secondary',
-      'bg-tertiary hover:bg-tertiary/80 focus:bg-tertiary/70':
-        color === 'tertiary',
-      'bg-quaternary hover:bg-quaternary/80 focus:bg-quaternary/70':
-        color === 'quaternary',
-      'bg-quinary hover:bg-quinary/80 focus:bg-quinary/70': color === 'quinary',
-      'transform hover:scale-105 focus:scale-105 transition-all duration-300 ease-in-out': true,
-      'rounded-t-lg': !noRounded,
-      'rounded-lg': rounded
-    }"
+    [ngClass]="getButtonClasses()"
     class="focus:outline-none text-white text-base font-sans w-full py-2 text-center"
   >
     {{ text }}
@@ -29,6 +17,7 @@ export class ButtonComponent {
   @Input() text: string = 'Button';
   @Input() type: string = 'button';
   @Input() color:
+    | 'red'
     | 'primary'
     | 'secondary'
     | 'tertiary'
@@ -37,4 +26,27 @@ export class ButtonComponent {
   @Input() noRounded: boolean = false;
   @Input() rounded: boolean = false;
   @Input() disabled: boolean = false;
+  @Input() customClass: string = '';
+
+  // Méthode pour calculer et retourner les classes dynamiques
+  getButtonClasses() {
+    return {
+      'bg-red-500 hover:bg-red-600 focus:bg-red-700': this.color === 'red',
+      'bg-primary hover:bg-primary/80 focus:bg-primary/70':
+        this.color === 'primary',
+      'bg-secondary hover:bg-secondary/80 focus:bg-secondary/70':
+        this.color === 'secondary',
+      'bg-tertiary hover:bg-tertiary/80 focus:bg-tertiary/70':
+        this.color === 'tertiary',
+      'bg-quaternary hover:bg-quaternary/80 focus:bg-quaternary/70':
+        this.color === 'quaternary',
+      'bg-quinary hover:bg-quinary/80 focus:bg-quinary/70':
+        this.color === 'quinary',
+      'transform hover:scale-105 focus:scale-105 transition-all duration-300 ease-in-out':
+        true,
+      'rounded-t-lg': !this.noRounded,
+      'rounded-lg': this.rounded,
+      [this.customClass]: !!this.customClass,
+    };
+  }
 }
