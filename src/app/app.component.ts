fa2bf869e3c6247d3de8cde50505a5826b9673bb
djoudj-dev <nedellec.julien.78@gmail.com>
2024-10-25 17:29:47 +0200

    Refactoriser le token pour la détéction des connexions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TokenService } from './core/token/token.service';
 import { AuthService } from './core/auth/auth.service';
 import { NavComponent } from './shared/components/header/navbar/nav.component';
@@ -7,6 +7,7 @@ import { RouterOutlet } from '@angular/router';
 import { AlertComponent } from './core/alert/alert.component';
 import { InactivityService } from './core/services/inactivity.service';
 import { AlertService } from './core/alert/service/alert.service';
+import { Subscription } from 'rxjs'; // Importer Subscription
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,9 @@ import { AlertService } from './core/alert/service/alert.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private inactivitySubscription!: Subscription; // Stocker la souscription
+
   constructor(
     private tokenService: TokenService,
     private authService: AuthService,
@@ -25,17 +28,29 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     const token = this.tokenService.getToken(); // Récupérer le token
+
+    // Vérifier si le token est présent, ce qui signifie que l'utilisateur est connecté
     if (token) {
-      // Vérifier si le token existe avant de vérifier son expiration
       if (this.tokenService.isTokenExpired()) {
-        this.authService.logout(); // Déconnecter l'utilisateur
+        this.authService.logout(); // Déconnecter l'utilisateur si le token est expiré
         this.alertService.showAlert(
           'Votre session a expiré, vous avez été déconnecté.'
         );
+      } else {
+        // Démarrer la surveillance de l'inactivité SEULEMENT pour les utilisateurs connectés
+        console.log(
+          "Utilisateur connecté, démarrage de la surveillance d'inactivité"
+        );
+        this.inactivityService.startMonitoring(); // Démarrer la surveillance
       }
+    } else {
+      console.log("Aucun token trouvé, l'utilisateur est un visiteur.");
+      // Ne pas démarrer la surveillance d'inactivité pour les visiteurs
     }
+  }
 
-    // Démarrer la surveillance de l'inactivité de l'utilisateur
-    this.inactivityService.startMonitoring(); // Le service commence à surveiller l'inactivité
+  ngOnDestroy(): void {
+    // Arrêter la surveillance lors de la destruction du composant
+    this.inactivityService.stopMonitoring();
   }
 }
