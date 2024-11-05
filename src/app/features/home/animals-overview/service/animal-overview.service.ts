9b8f8be734bf0ebd7bab8aaa62eed38d2207e2d6
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-05 22:34:11 +0100

    Refactor la gestion des animaux : mise à jour des services et correction des chemins d'importation pour une meilleure organisation et gestion des images.

--- src/app/features/home/animals-overview/service/animal-overview.service.ts
+++ src/app/features/home/animals-overview/service/animal-overview.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
-import { Animal } from '../../../../core/models/animal.model';
+import { Animal } from '../../../admin-dashboard/animal-management/model/animal.model';
+import { environment } from '../../../../environments/environment.development'; // Corrigez le chemin ici
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimalOverviewService {
-  private imageBaseUrl = `${environment.apiUrl}`;
+  private apiUrl = `${environment.apiUrl}/api/animals`;
+  private uploadsUrl = `${environment.apiUrl}/uploads`; // Base URL pour les images
 
   constructor(private http: HttpClient) {}
 
   /**
-   * Récupère la liste des animaux.
+   * Récupère la liste des animaux depuis l'API.
    */
-  getAnimals() {
-    return this.http.get<Animal[]>(`${this.imageBaseUrl}/animals`);
+  getAnimals(): Observable<Animal[]> {
+    console.log('Fetching animals from:', this.apiUrl); // Ajoutez cette ligne pour vérifier l'URL
+    return this.http.get<Animal[]>(this.apiUrl).pipe(
+      map((animals) =>
+        animals.map((animal) => ({
+          ...animal,
+          images: animal.images ? `${this.uploadsUrl}/${animal.images}` : '', // Ajouter l'URL complète de l'image
+        }))
+      ),
+      tap((animals) => console.log('Fetched animals:', animals)), // Ajoutez cette ligne pour vérifier les données reçues
+      catchError((error) => {
+        console.error('Error fetching animals:', error);
+        return throwError(error);
+      })
+    );
   }
 }
