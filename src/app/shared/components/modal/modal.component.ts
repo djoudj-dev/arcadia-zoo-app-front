3abf4b44c0de73094b7496224790efc04f52c7b1
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-14 18:50:16 +0100

    améliorer le formulaire d'avis utilisateur : mise à jour du style, validation des champs et gestion du modal + intégration de Toast pour les messages

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -11,26 +11,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
       <div
         class="bg-secondary w-full sm:max-w-lg mx-auto p-6 rounded-lg shadow-lg relative"
       >
-        <!-- Close button -->
-        <button
-          (click)="closeModal()"
-          class="absolute top-2 right-2 text-quinary hover:text-gray-600"
-        >
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
         <ng-content />
       </div>
     </div>
