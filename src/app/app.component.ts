8dc812a6ad63e63f726f9251f475fbda72737416
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-15 10:31:46 +0100

    Supprimer le composant Alert et le service associé, remplaçant son utilisation par ToastService pour les alertes utilisateur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,47 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Subscription } from 'rxjs'; // Importer Subscription
-import { AlertComponent } from './core/alert/alert.component';
-import { AlertService } from './core/alert/service/alert.service';
+import { Subscription } from 'rxjs';
 import { AuthService } from './core/auth/auth.service';
 import { InactivityService } from './core/services/inactivity.service';
 import { TokenService } from './core/token/token.service';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavComponent } from './shared/components/header/navbar/nav.component';
 import { ToastComponent } from './shared/components/toast/toast.component';
+import { ToastService } from './shared/services/toast.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    NavComponent,
-    FooterComponent,
-    RouterOutlet,
-    AlertComponent,
-    ToastComponent,
-  ],
+  imports: [NavComponent, FooterComponent, RouterOutlet, ToastComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  private inactivitySubscription!: Subscription; // Stocker la souscription
+  private inactivitySubscription!: Subscription;
 
   constructor(
     private tokenService: TokenService,
     private authService: AuthService,
     private inactivityService: InactivityService,
-    private alertService: AlertService // Injecter le service d'alerte
+    private toastService: ToastService
   ) {}
 
   ngOnInit(): void {
-    const token = this.tokenService.getToken(); // Récupérer le token
+    const token = this.tokenService.getToken();
 
-    // Vérifier si le token est présent, ce qui signifie que l'utilisateur est connecté
     if (token) {
       if (this.tokenService.isTokenExpired()) {
-        this.authService.logout(); // Déconnecter l'utilisateur si le token est expiré
-        this.alertService.setAlert(
+        this.authService.logout();
+        this.toastService.showError(
           'Votre session a expiré, vous avez été déconnecté.'
         );
       } else {
-        // Démarrer la surveillance de l'inactivité SEULEMENT pour les utilisateurs connectés
-        this.inactivityService.startMonitoring(); // Démarrer la surveillance
+        this.inactivityService.startMonitoring();
       }
     }
   }
 
   ngOnDestroy(): void {
-    // Arrêter la surveillance lors de la destruction du composant
     this.inactivityService.stopMonitoring();
   }
 }
