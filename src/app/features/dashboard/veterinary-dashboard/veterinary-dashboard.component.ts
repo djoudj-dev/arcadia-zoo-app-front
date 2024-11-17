1fe76766331a70dfd63a419f4caeaa3c656e64f2
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-17 15:24:20 +0100

    Supprimé des fichiers CSS et HTML non utilisés, ainsi que le service et les composants liés au tableau de bord vétérinaire

---
+++ src/app/features/dashboard/veterinary-dashboard/veterinary-dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-veterinary-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './veterinary-dashboard.component.html',
+})
+export class VeterinaryDashboardComponent {}
