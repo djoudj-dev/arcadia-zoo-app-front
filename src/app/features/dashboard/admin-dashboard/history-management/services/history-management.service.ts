93b4e001e244a49493463bbd8dabf5cf0c835674
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-19 00:08:12 +0100

    Ajout de la gestion de l'historique des commentaires sur les habitats dans le tableau de bord administratif avec amélioration de l'interface utilisateur

---
+++ src/app/features/dashboard/admin-dashboard/history-management/services/history-management.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { HabitatComment } from 'app/features/dashboard/veterinary-dashboard/habitat-comment/habitat-comment/model/habitat-comment.model';
+import { CommentStatusService } from 'app/shared/services/comment-status.service';
+import { environment } from 'environments/environment.development';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HabitatHistoryService {
+  private apiUrl = `${environment.apiUrl}/api/habitat-comment`;
+  private http = inject(HttpClient);
+  private commentStatusService = inject(CommentStatusService);
+
+  constructor() {}
+
+  getHabitatCommentHistory(): Observable<HabitatComment[]> {
+    return this.http.get<HabitatComment[]>(this.apiUrl).pipe(
+      tap((comments) => {
+        console.log('Tous les commentaires récupérés:', comments);
+      })
+    );
+  }
+
+  markCommentAsResolved(commentId: string) {
+    const url = `${this.apiUrl}/${commentId}/resolve`;
+    console.log('Tentative de résolution du commentaire:', commentId);
+
+    return this.http.patch<HabitatComment>(url, {}).pipe(
+      tap({
+        next: (response) => {
+          console.log('Commentaire marqué comme résolu:', response);
+          this.commentStatusService.emitCommentStatusChange(response);
+        },
+        error: (error) => console.error('Erreur lors de la résolution:', error),
+      })
+    );
+  }
+
+  reopenComment(commentId: string) {
+    const url = `${this.apiUrl}/${commentId}/reopen`;
+    console.log('Tentative de réouverture du commentaire:', commentId);
+
+    return this.http.patch<HabitatComment>(url, {}).pipe(
+      tap({
+        next: (response) => {
+          console.log('Commentaire rouvert:', response);
+          this.commentStatusService.emitCommentStatusChange(response);
+        },
+        error: (error) =>
+          console.error('Erreur lors de la réouverture:', error),
+      })
+    );
+  }
+}
