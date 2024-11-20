753e649e1ea5747515dd2803ea66460b5f9a86d3
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-20 20:24:54 +0100

    refactor du composant CountResource : amélioration de l'interface, ajout de gestion des erreurs et documentation des méthodes essentielles

--- src/app/features/dashboard/admin-dashboard/stats-board/counts-resource/count-resource.component.ts
+++ src/app/features/dashboard/admin-dashboard/stats-board/counts-resource/count-resource.component.ts
@@ -1,35 +1,52 @@
 // src/app/admin-dashboard/stats/stats.component.ts
 import { Component, OnInit } from '@angular/core';
+import { ToastService } from 'app/shared/components/toast/services/toast.service';
 import { CountResourceService } from './services/count-resource.service';
+import { ToastComponent } from '../../../../../shared/components/toast/toast.component';
 
+/**
+ * Composant d'affichage des statistiques des ressources
+ * Affiche les compteurs pour les différentes ressources du zoo
+ */
 @Component({
   standalone: true,
   selector: 'app-count-resource',
+  imports: [ToastComponent],
   templateUrl: './count-resource.component.html',
 })
 export class CountResourceComponent implements OnInit {
-  constructor(public countResourceService: CountResourceService) {}
+  constructor(
+    public countResourceService: CountResourceService,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit() {
     this.loadCountResource();
   }
 
+  /**
+   * Charge les statistiques depuis le service
+   * Met à jour les compteurs et affiche les notifications
+   */
   loadCountResource() {
     this.countResourceService.getStats().subscribe({
       next: () => {
-        console.log(
-          'Statistiques mises à jour:',
-          this.countResourceService.totalAnimals(),
-          this.countResourceService.totalHabitats(),
-          this.countResourceService.totalServices(),
-          this.countResourceService.totalEmploye(),
-          this.countResourceService.totalVet()
-        );
+        console.log('Statistiques mises à jour:', {
+          animaux: this.countResourceService.totalAnimals(),
+          habitats: this.countResourceService.totalHabitats(),
+          services: this.countResourceService.totalServices(),
+          employes: this.countResourceService.totalEmploye(),
+          veterinaires: this.countResourceService.totalVet(),
+        });
+        this.toastService.showSuccess('Statistiques chargées avec succès');
       },
       error: (error) => {
         console.error(
           'Erreur lors de la récupération des statistiques:',
           error
+        );
+        this.toastService.showError(
+          'Erreur lors du chargement des statistiques'
         );
       },
     });
