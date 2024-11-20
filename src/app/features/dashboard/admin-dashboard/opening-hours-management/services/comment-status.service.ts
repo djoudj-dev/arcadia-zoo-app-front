f78b46cb551487a6181be8d54f60782418cc4899
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-20 19:01:02 +0100

    refactor du service CommentStatus : ajout de documentation et gestion des notifications de changement de statut des commentaires

--- src/app/features/dashboard/admin-dashboard/opening-hours-management/services/comment-status.service.ts
+++ src/app/features/dashboard/admin-dashboard/opening-hours-management/services/comment-status.service.ts
@@ -2,14 +2,24 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HabitatComment } from '../../../veterinary-dashboard/habitat-comment/habitat-comment/model/habitat-comment.model';
 
+/**
+ * Service de gestion du statut des commentaires
+ * Gère les notifications de changement d'état des commentaires
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class CommentStatusService {
+  /** Subject pour la gestion des changements de statut */
   private commentStatusChanged = new Subject<HabitatComment>();
 
+  /** Observable public pour s'abonner aux changements de statut */
   commentStatusChanged$ = this.commentStatusChanged.asObservable();
 
+  /**
+   * Émet un événement de changement de statut
+   * @param comment Commentaire dont le statut a changé
+   */
   emitCommentStatusChange(comment: HabitatComment) {
     this.commentStatusChanged.next(comment);
   }
