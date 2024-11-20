89688ec2a70a9d529aaed4532895d7548c8d42fb
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-20 20:30:03 +0100

    refactor du composant Stats : ajout du composant CountResource dans les imports et mise à jour de l'interface utilisateur pour les stats

--- src/app/features/dashboard/admin-dashboard/stats-board/stats.component.ts
+++ src/app/features/dashboard/admin-dashboard/stats-board/stats.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CountResourceComponent } from './counts-resource/count-resource.component';
 
+/**
+ * Composant principal des statistiques
+ * Agrège et affiche les différents composants de statistiques
+ */
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
   standalone: true,
-  imports: [],
+  imports: [CountResourceComponent],
 })
 export class StatsComponent {}
