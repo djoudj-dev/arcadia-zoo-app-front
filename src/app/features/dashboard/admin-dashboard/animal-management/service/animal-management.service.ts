0146f5b6103ea7b6aa1b92148799b0b1b0d595f0
djoudj-dev <nedellec.julien.78@gmail.com>
2024-12-25 18:08:41 +0100

    fix(animal-management): Correct environment import

--- src/app/features/dashboard/admin-dashboard/animal-management/service/animal-management.service.ts
+++ src/app/features/dashboard/admin-dashboard/animal-management/service/animal-management.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, tap, throwError } from 'rxjs';
-import { environment } from '../../../../../../environments/environment.development';
+import { environment } from '../../../../../../environments/environment';
 import { AnimalService } from '../../../../animal/service/animal.service';
 import { Animal } from '../model/animal.model';
 
