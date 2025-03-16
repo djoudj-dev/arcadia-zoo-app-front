882938ad19556f81df474e0d722363cfad44fbef
djoudj-dev <nedellec.julien.78@gmail.com>
2025-03-16 14:39:43 +0100

    feat: ajouter de nouveaux composants pour la gestion des services et des avis utilisateurs

---
+++ dist/arcadia-zoo-app-front/browser/chunk-WQT2M6SU.js
@@ -0,0 +1,1 @@
+import{_ as m,j as a}from"./chunk-RMHEMJYH.js";var o=class t{commentStatusChanged=new a;commentStatusChanged$=this.commentStatusChanged.asObservable();emitCommentStatusChange(e){this.commentStatusChanged.next(e)}static \u0275fac=function(n){return new(n||t)};static \u0275prov=m({token:t,factory:t.\u0275fac,providedIn:"root"})};export{o as a};
